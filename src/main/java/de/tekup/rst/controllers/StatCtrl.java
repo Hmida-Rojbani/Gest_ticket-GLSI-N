266cb0b463945b4042f5d3e24792e4567d0d521d
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-25 19:26:19 +0100

    stat service

---
+++ src/main/java/de/tekup/rst/controllers/StatCtrl.java
@@ -0,0 +1,26 @@
+package de.tekup.rst.controllers;
+
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.tekup.rst.dto.models.MetDTO;
+import de.tekup.rst.services.StatService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class StatCtrl {
+	
+	private StatService statService;
+	
+	@GetMapping("/api/stats/plat/{deb}/{fin}")
+	public MetDTO getPlatPlusAchetee(@PathVariable String deb,@PathVariable String fin) {
+		LocalDate debDate = LocalDate.parse(deb);
+		LocalDate finDate = LocalDate.parse(fin);
+		return statService.platPlusAchetee(debDate, finDate);
+	}
+
+}
---
+++ src/main/java/de/tekup/rst/services/StatService.java
@@ -0,0 +1,53 @@
+package de.tekup.rst.services;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import de.tekup.rst.dto.models.MetDTO;
+import de.tekup.rst.entities.MetEntity;
+import de.tekup.rst.entities.Plat;
+import de.tekup.rst.repositories.MetRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class StatService {
+	
+	private MetRepository metRepository;
+	private ModelMapper mapper;
+	
+	public MetDTO platPlusAchetee(LocalDate debut, LocalDate fin) {
+		
+		List<MetEntity> mets = metRepository.findAll();
+		System.out.println(mets);
+		//locate Plat objects
+		mets.removeIf(met -> !(met instanceof Plat));
+		System.out.println(mets);
+		
+		for (MetEntity metEntity : mets) {
+			//remove tickets out of period
+			metEntity.getTickets()
+			.removeIf(t -> t.getDateTime().toLocalDate().isBefore(debut) ||
+					t.getDateTime().toLocalDate().isAfter(fin));
+		}
+		
+		int max = -1;
+		MetEntity entity = null;
+		
+		for (MetEntity metEntity : mets) {
+			if (metEntity.getTickets().size() > max) {
+				max = metEntity.getTickets().size();
+				entity = metEntity;
+			}
+		}
+		MetDTO dto = mapper.map(entity, MetDTO.class);
+		dto.setType("Plat");
+		return dto;
+	}
+	
+	
+
+}
